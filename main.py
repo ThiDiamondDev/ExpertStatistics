ae2e7272ee59362ba194570576990798c130c040
Thiago <thi.dimond1@gmail.com>
2023-11-06 19:26:24 -0300

    Fix root window centering.

--- main.py
+++ main.py
@@ -9,14 +9,10 @@ def main():
     mt5 = MT5()
     # Set the geometry of frame
     w, h = root.winfo_screenwidth(), root.winfo_screenheight()
-    root.geometry("%dx%d+0+0" % (w, h))
     root.state("zoomed")
-    root.eval(
-        "tk::PlaceWindow %s center" % root.winfo_toplevel()
-    )  # center the window on the screen
 
     ui = GUI(root, mt5)
-
+    ui.center_window(root, w=w, h=h)
     root.protocol("WM_DELETE_WINDOW", lambda: [mt5.shutdown(), root.destroy()])
     ui.plot_data()
     root.mainloop()  # start the main loop of the GUI
